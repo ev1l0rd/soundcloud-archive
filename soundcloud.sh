744c53b426d66446c4644ed8e611ec35330495c8
Ev1l0rd <valentijnvermeij@gmail.com>
2017-07-28 12:33:11 +0200

    new function: dependency_check()

--- soundcloud.sh
+++ soundcloud.sh
@@ -17,4 +17,18 @@ function dependency_check() {
 	command -v pip2 >/dev/null 2>&1 || { echo >&2 "PIP for Python 2.7 is not installed. Install pip."; exit 1; }
 	command -v ia >/dev/null 2>&1 || { echo >&2 "Internet Archive tools not installed."; IA_TOOLS_SETUP="yes"; }
 	command -v youtube-dl >/dev/null 2>&1 || { echo >&2 "Youtube-DL not installed."; YOUTUBE-DL_SETUP="yes"; }
+	
 }
+
+function dependency_fix() {
+	if [["YOUTUBE-DL_SETUP" == "yes"]] {
+		sudo pip install youtube-dl
+		echo "youtube-dl installed. Continuing."
+	}
+	if [["IA_TOOLS_SETUP" == "yes"]] {
+		sudo pip install internetarchive
+		echo "Internet Archive Tools installed. Please run 'ia configure', then re-run this script."
+		exit 0
+	}
+}
+
