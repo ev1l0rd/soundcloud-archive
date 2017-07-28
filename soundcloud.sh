dc820692fc49f1200c92c2c3b19acf7cd26c5de7
Ev1l0rd <valentijnvermeij@gmail.com>
2017-07-28 12:39:50 +0200

    new function: create_temp_dir()
    also created clean() function that is called upon EXIT signal to ensure proper cleanup, even if the script borks

--- soundcloud.sh
+++ soundcloud.sh
@@ -14,7 +14,7 @@ EOF
 }
 
 function dependency_check() {
-	command -v pip2 >/dev/null 2>&1 || { echo >&2 "PIP for Python 2.7 is not installed. Install pip."; exit 1; }
+	command -v pip2 >/dev/null 2>&1 || { echo >&2 "PIP for Python 2.7 is not installed. Install pip for Python 2.7, then re-run this script."; exit 1; }
 	command -v ia >/dev/null 2>&1 || { echo >&2 "Internet Archive tools not installed."; IA_TOOLS_SETUP="yes"; }
 	command -v youtube-dl >/dev/null 2>&1 || { echo >&2 "Youtube-DL not installed."; YOUTUBE-DL_SETUP="yes"; }
 	
@@ -32,3 +32,20 @@ function dependency_fix() {
 	}
 }
 
+function create_temp_dir() {
+	#shortened version of https://stackoverflow.com/a/34676160/4666756
+	curdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+	temp_dir=`mktemp -d -p "$curdir"`
+	if [[ ! "$temp_dir" || ! -d "$temp_dir" ]]; then
+	  echo "Could not create temporary directory."
+	  exit 1
+	fi
+}
+
+function clean {
+	rm -rf $temp_dir
+	echo "Temporary directory removed"
+}
+
+
+trap clean EXIT
